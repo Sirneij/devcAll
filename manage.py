e0c6517ddf02e324af31037aae9189d1ab5564de
John Idogun <sirneij@gmail.com>
2020-12-04 15:41:11 +0100

    Configured the heroku depeloyment settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devc.settings')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devc.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devc.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
